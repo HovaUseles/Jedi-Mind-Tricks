6aee2b603331009790377b50f9ed94008a0ef4af
HovaUseles <bocaj.nielsen15@gmail.com>
2021-09-03 09:51:32 +0200

    Add project files.

---
+++ Jedi Mind Tricks/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind_Tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>();
+
+            people.Add("John", 44);
+            people.Add("Simon", 54);
+            people.Add("Gelmmi", 12);
+            people["Bo"] = 33;
+
+
+            Console.WriteLine(people.ElementAt(0).Key + ", " + people.ElementAt(0).Value);
+
+            people.Remove("Bo");
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.Key}, {person.Value}");
+            }
+        }
+    }
+}
