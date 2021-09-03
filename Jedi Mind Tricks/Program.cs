a7a8e5b663fe95cb4f28fbccfd52b45494d8a38c
HovaUseles <bocaj.nielsen15@gmail.com>
2021-09-03 09:53:34 +0200

    Finished

--- Jedi Mind Tricks/Program.cs
+++ Jedi Mind Tricks/Program.cs
@@ -12,14 +12,18 @@ namespace Jedi_Mind_Tricks
         {
             Dictionary<string, int> people = new Dictionary<string, int>();
 
+            
             people.Add("John", 44);
             people.Add("Simon", 54);
             people.Add("Gelmmi", 12);
+
+            // Adds new entry if it does not exits
             people["Bo"] = 33;
 
-
+            // Using ElementAt method to make the dictionary act like an list with indexes
             Console.WriteLine(people.ElementAt(0).Key + ", " + people.ElementAt(0).Value);
-
+            
+            // Removes an element with the key Bo
             people.Remove("Bo");
 
             foreach (var person in people)
